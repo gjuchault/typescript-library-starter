39cf97ab6cb9bf5648428f4fa08b757bc1a422d3
Gabriel Juchault <gabriel.juchault@gmail.com>
2023-07-23 12:54:42 +0200

    chore(build): return to esbuild

---
+++ scripts/build.ts
@@ -0,0 +1,48 @@
+import path from "node:path";
+import url from "node:url";
+import fs from "node:fs/promises";
+import { globbyStream } from "globby";
+import { build as esbuild } from "esbuild"
+
+const srcPath = path.join(process.cwd(), "src");
+const buildPath = path.join(process.cwd(), "build");
+
+async function buildFile(
+  filePath: string,
+) {
+  return esbuild({
+    platform: "node",
+    target: "node18",
+    format: "esm",
+    nodePaths: [srcPath],
+    sourcemap: true,
+    external: [],
+    entryPoints: [path.join(srcPath, filePath)],
+    outdir: path.join(buildPath, path.dirname(filePath)),
+  });
+}
+
+async function build() {
+  await fs.rm(buildPath, { recursive: true });
+
+  const filesStream = globbyStream('**/*.ts', {
+    cwd: srcPath,
+    onlyFiles: true,
+    ignore: ["__tests__"]
+  });
+
+  for await (const filePath of filesStream) {
+    if (typeof filePath !== "string") {
+      throw new TypeError("Unexpected file type");
+    }
+    
+    await buildFile(filePath)
+  }
+}
+
+if (import.meta.url.startsWith("file:")) {
+  if (process.argv[1] === url.fileURLToPath(import.meta.url)) {
+    await build();
+  }
+}
+
