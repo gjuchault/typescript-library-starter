d437d612624e7979fea6052084e78d932d38ab8c
Gabriel JUCHAULT <gabriel.juchault@gmail.com>
2023-12-18 18:27:55 +0100

    feat: bundle library (#610)
    * chore(vscode): improve run test task experience
    
    * chore(vscode): support comments in json configuration files
    
    * feat(dts): use api-extractor to only emit exported declaration files
    
    * feat(build): use bundling
    
    switched back to esbuild's bundling, which goes well with api-extractor
    
    BREAKING CHANGE: switched back to esbuild's bundling, which goes well with api-extractor
    
    * chore(npm): commit package-lock
    
    * fix-up: tsx over .ts files
    
    * fix: switch to swc-register to be able to run coverage
    
    * feat: use esbuild-register instead

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,12 +1,11 @@
 import path from "node:path";
 import url from "node:url";
-import { globbyStream } from "globby";
 import { build as esbuild } from "esbuild";
 
 const srcPath = path.join(process.cwd(), "src");
 const buildPath = path.join(process.cwd(), "build");
 
-async function buildFile(filePath: string) {
+async function build() {
   return esbuild({
     platform: "node",
     target: "node21",
@@ -14,31 +13,14 @@ async function buildFile(filePath: string) {
     nodePaths: [srcPath],
     sourcemap: true,
     external: [],
-    entryPoints: [path.join(srcPath, filePath)],
-    outdir: path.join(buildPath, path.dirname(filePath)),
+    bundle: true,
+    entryPoints: [path.join(srcPath, "index.ts")],
+    outdir: buildPath,
   });
-}
-
-async function build({ includeTests = false }: { includeTests?: boolean }) {
-  const filesStream = globbyStream("**/*.ts", {
-    cwd: srcPath,
-    onlyFiles: true,
-    ignore: includeTests ? [] : ["__tests__"],
-  });
-
-  for await (const filePath of filesStream) {
-    if (typeof filePath !== "string") {
-      throw new TypeError("Unexpected file type");
-    }
-
-    await buildFile(filePath);
-  }
 }
 
 if (import.meta.url.startsWith("file:")) {
   if (process.argv[1] === url.fileURLToPath(import.meta.url)) {
-    await build({
-      includeTests: process.argv.includes("--tests"),
-    });
+    await build();
   }
 }
