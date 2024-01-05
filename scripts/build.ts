80c0be06c354a47a7b137dfbb0da3fcd1c2d4d10
Gabriel Juchault <gabriel.juchault@gmail.com>
2024-01-05 17:21:49 +0100

    fix(build): dynamic require banner

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,11 +1,14 @@
 import path from "node:path";
 import url from "node:url";
 import { build as esbuild } from "esbuild";
+import { randomUUID } from "node:crypto";
 
 const srcPath = path.join(process.cwd(), "src");
 const buildPath = path.join(process.cwd(), "build");
 
 async function build() {
+  const buildId = randomUUID().replace(/-/g, "");
+
   return esbuild({
     platform: "node",
     target: "node21",
@@ -17,11 +20,15 @@ async function build() {
     entryPoints: [path.join(srcPath, "index.ts")],
     banner: {
       js: `
-            import { createRequire } from 'module';
-            import { fileURLToPath } from 'url';
-            const require = createRequire(import.meta.url);
-            const __filename = fileURLToPath(import.meta.url);
-            const __dirname = path.dirname(__filename);
+            import { createRequire as createRequire${buildId} } from 'module';
+            import { fileURLToPath as fileURLToPath${buildId} } from 'url';
+            import { dirname as dirname${buildId} } from 'path';
+
+            // using var here to allow subsequent override by authors of this
+            // library that would be using the same ESM trick
+            var require = createRequire${buildId}(import.meta.url);
+            var __filename = fileURLToPath${buildId}(import.meta.url);
+            var __dirname = dirname${buildId}(__filename);
       `,
     },
     outdir: buildPath,
