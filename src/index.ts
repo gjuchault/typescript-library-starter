f6b97f2ee84a4b2b8bc238bc6c8dbb6216ae3a94
Gabriel Juchault <gabriel.juchault@gmail.com>
2025-07-08 23:01:28 +0200

    chore(ts): build with tsgo, requires .js extension

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { bar } from "./bar.ts";
-import { foo } from "./foo.ts";
-import { baz } from "./nested/baz.ts";
+import { bar } from "./bar.js";
+import { foo } from "./foo.js";
+import { baz } from "./nested/baz.js";
 
 export function foobar(a: number, b: number) {
 	return foo().repeat(a).length + bar().repeat(b).length + baz().length;
