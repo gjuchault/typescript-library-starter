4d9f6bf56eba46fe4fcec8e0b0ffe2cc06b16706
Gabriel Juchault <gabriel.juchault@gmail.com>
2025-07-08 23:18:02 +0200

    chore: support extensions

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { bar } from "./bar.js";
-import { foo } from "./foo.js";
-import { baz } from "./nested/baz.js";
+import { bar } from "./bar.ts";
+import { foo } from "./foo.ts";
+import { baz } from "./nested/baz.ts";
 
 export function foobar(a: number, b: number) {
 	return foo().repeat(a).length + bar().repeat(b).length + baz().length;
